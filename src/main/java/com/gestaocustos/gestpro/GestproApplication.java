2b224cd9c20e11081e3c003b768a3e0509cc6a6b
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-01-28 13:45:01 -0300

    Initialize project with Maven, Spring Boot, and necessary configurations

---
+++ src/main/java/com/gestaocustos/gestpro/GestproApplication.java
@@ -0,0 +1,13 @@
+package com.gestaocustos.gestpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaocustos/gestpro/GestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaocustos.gestpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
